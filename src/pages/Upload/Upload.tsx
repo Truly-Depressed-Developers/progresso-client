1615af979745829db1c16d14b0ae7104bbe86b94
Spookyless <spookyless.info@gmail.com>
2023-01-14 22:39:37 +0100

    Upload jest ladny

--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -1,13 +1,17 @@
-import { FormControl, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import "./Upload.scss";
+
+import { FormControl, InputLabel, MenuItem, Paper, Select, Typography } from "@mui/material";
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import { Settings } from "../../settings";
+import { CircularProgress } from '@mui/material';
 
 type Props = {}
 
 const Upload = (props: Props): JSX.Element => {
 
     const [isUploading, setIsUploading] = useState(false);
+    const [isFinishedUploading, setIsFinishedUploading] = useState(false);
 
     const onDrop = useCallback((acceptedFiles: File[]) => {
         if (acceptedFiles.length === 0) {
@@ -15,6 +19,7 @@ const Upload = (props: Props): JSX.Element => {
         }
 
         setIsUploading(true);
+        setIsFinishedUploading(false);
 
         const data = new FormData();
 
@@ -22,36 +27,62 @@ const Upload = (props: Props): JSX.Element => {
             data.append(`file-${i}`, file, file.name);
         });
 
-        fetch(Settings.serverUrl + "fileUpload", {
+        fetch(Settings.serverUrl + "file", {
             method: "POST", body: data
-        });
+        })
+            .then(() => {
+                setIsUploading(false)
+                setIsFinishedUploading(true);
+            });
 
     }, []);
 
     const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop, maxFiles: 1 });
 
+    const shouldReact = !(isUploading || isFinishedUploading);
+
     return (
-        <FormControl>
+        <FormControl id="upload-form">
             <Typography variant="h3">
                 Upload
             </Typography>
 
-            {isUploading ?
-                <p></p> :
-                <div
-                    {...getRootProps()}
-                    style={{ border: "2px dashed gray", borderRadius: 8, cursor: "pointer" }}
-                >
-                    <input {...getInputProps()}
-                    />
-                    {
-                        isDragActive ?
-                            <p>Drop here</p> :
-                            <p>Drag some files to upload them</p>
-                    }
-                </div>
-            }
+            <Paper
+                elevation={2}
+                style={{ width: "80vw", height: "50vh", padding: 32 }}
+            >
 
+                <div
+                    className="files-drop"
+                    {...getRootProps()}
+                    style={{ transition: "background-color 0.25s", cursor: (shouldReact ? "pointer" : "default"), backgroundColor: ((shouldReact && isDragActive) ? "rgba(0,255,0,0.2)" : "rgba(0,0,0,0.04)") }}
+                >
+                    <input
+                        {...getInputProps()}
+                        disabled={shouldReact === false}
+                    />
+
+                    {isUploading === false && isFinishedUploading === false &&
+                        (isDragActive ?
+                            <span>Drop here!</span> :
+                            <span>Drag and drop file(s) here</span>
+                        )
+                    }
+
+                    {isUploading === true && isFinishedUploading === false &&
+                        <>
+                            <CircularProgress />
+                            <span>Uploading...</span>
+                        </>
+                    }
+
+                    {isUploading === false && isFinishedUploading === true &&
+                        <span>File(s) uploaded!</span>
+                    }
+
+                </div>
+
+            </Paper>
         </FormControl>
     );
 }
