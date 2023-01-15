d0c6342833043139469508b9d21bee0abf4cc2c4
Krzysztof Wrona <rubikon02@gmail.com>
2023-01-15 03:33:26 +0100

    Add achievement name in tooltip

--- src/components/Achievement/Achievement.tsx
+++ src/components/Achievement/Achievement.tsx
@@ -1,21 +1,28 @@
 import './Achievement.scss';
 import { Tooltip } from '@mui/material';
+import { Fragment, } from 'react';
+import { HtmlTooltip } from '../../components/HtmlTooltip';
+import Typography from '@mui/material/Typography';
 
 type Props = {
     iconUrl: string,
+    name: string,
     description: string,
 }
 
 export const Achievement = (props: Props): JSX.Element => {
     return (
-        <Tooltip
+        <HtmlTooltip
             title={
-                <span className='achievement-tooltip'>{props.description}</span>
+                <Fragment>
+                    <Typography className="achievement-tooltip-name">{props.name}</Typography>
+                    <span className='achievement-tooltip-description'>{props.description}</span>
+                </Fragment>
             }
         >
             <div className="achievement">
                 <img src={props.iconUrl} />
             </div>
-        </Tooltip>
+        </HtmlTooltip>
     )
 }
